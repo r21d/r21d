b65509662cc02a2614b848b0b38aea29c4c37b16
r21d <jepus@live.fi>
2024-08-06 21:40:20 +0300

    ddss

--- app.rb
+++ app.rb
@@ -22,3 +22,41 @@ end
 get "/list" do
   redirect "https://www.youtube.com/watch?v=QblWYWmXOQ4&list=PL6sZpQz3MZtnG4B2W5RlaXUKUkr6catIr"
 end
+
+require 'json'
+
+get "/gowithit" do
+
+  fl = 'lyricsbi.json'
+  fa = 'ASFA.json'
+  fo = 'ogjssonfix.json'
+
+  fld = JSON.parse(File.read(fl))
+  fad = JSON.parse(File.read(fa))
+  fod = JSON.parse(File.read(fo))
+
+  cd = {}
+
+  fod.each do |key, item|
+    cd[key] = {
+      'id' => item['id'],
+      'sub' => item['sub'],
+      'obj' => item['obj'],
+      'tag' => item['tag'],
+      'img' => item['img'],
+      'txt' => item['txt']
+    }
+  end
+
+  fad.each do |key, item|
+    cd[key]['sum'] = item['sum']
+    cd[key]['rad'] = item['rad']
+  end
+
+  fld.each do |key, item|
+    cd[key].merge!(item)  
+  end
+
+    erb :gowithit, locals: { cd: cd.to_json } 
+
+end
