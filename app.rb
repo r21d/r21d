7b296a43b23d6ac6fe70047e6dcb9c33b19177ed
r21d <jepus@live.fi>
2024-08-06 09:00:10 +0300

    rew

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ set :port, port
 
 get '/h/:filename' do
   filename = params[:filename].split(':')
-  file_path = File.join("/public/", "#{filename[0]}.#{filename[1]}")
+  file_path = File.join("public", "#{filename[0]}.#{filename[1]}")
   if File.exist?(file_path)
     send_file file_path
   else
@@ -17,7 +17,6 @@ end
 
 get "/" do
   redirect "https://www.youtube.com/channel/UCkfI4g-ztKQZa3bB_QX9gBw?sub_confirmation=1"
-
 end
 
 get "/list" do
