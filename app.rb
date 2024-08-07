e8b3780f405783d0f7acf6979dd87b8d7dd11e80
r21d <jepus@live.fi>
2024-08-07 17:23:47 +0300

    wow f comments

--- app.rb
+++ app.rb
@@ -28,26 +28,32 @@ get "/list" do
   redirect "https://www.youtube.com/watch?v=QblWYWmXOQ4&list=PL6sZpQz3MZtnG4B2W5RlaXUKUkr6catIr"
 end
 
+get '/wow' do
+  json_file_path = File.join(settings.public_folder, 'o.json') 
+  begin
+    json_data = JSON.parse(File.read(json_file_path))
+  rescue Errno::ENOENT => e
+    status 404
+    return "File not found: #{e.message}"
+  rescue JSON::ParserError => e
+    status 500
+    return "Invalid JSON data: #{e.message}"
+  end
+  erb :gow, locals: { data: json_data } 
+end
 
 get "/gowithit" do
-  # Fetch playlist data from YouTube API
-  playlist_id = "PL6sZpQz3MZtnG4B2W5RlaXUKUkr6catIr "  # Replace with your actual playlist ID
-  api_key = "AIzaSyCMkYOzj-pE5BlUmdnJBStvsNtdOalHKMo"           # Replace with your YouTube Data API key
+  playlist_id = "PL6sZpQz3MZtnG4B2W5RlaXUKUkr6catIr" 
+  api_key = "AIzaSyCMkYOzj-pE5BlUmdnJBStvsNtdOalHKMo"
   url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=#{playlist_id}&key=#{api_key}&maxResults=50"   
-  # Get up to 50 videos
-
   response = open(url)
   data = JSON.parse(response.read)
-
-  # Extract video information
   videos = data['items'].map do |item|
     {
       title: item['snippet']['title'],
       videoId: item['snippet']['resourceId']['videoId'],
-      thumbnail: item['snippet']['thumbnails']['medium']['url']  # Choose appropriate thumbnail size
+      thumbnail: item['snippet']['thumbnails']['medium']['url'] 
     }
   end
-
-  # Pass video data to the view
   erb :playlist, locals: { videos: videos }
 end
